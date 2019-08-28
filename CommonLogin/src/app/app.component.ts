dacb59e8a20fde7cff10508a137457e8c49307c9
Navrose Singh <navrosesingh@gmail.com>
2019-08-28 12:43:45 +0530

    UIKIT working

--- CommonLogin/src/app/app.component.ts
+++ CommonLogin/src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+declare var UIkit: any;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'CommonLogin';
+  ngOnInit() {
+  }
+  showAlert(): void {
+    UIkit.modal.alert('UIkit alert!');
+  }
 }
